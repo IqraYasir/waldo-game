9a796dc8398c6f364906f232f2f9b1ee436fa2c0
IqraYasir <iqrayasir0906@gmail.com>
2024-06-04 18:54:49 +0100

    Create dropdowncomponent, add selection state

--- frontend/src/components/Dropdown.jsx
+++ frontend/src/components/Dropdown.jsx
@@ -1,27 +1,51 @@
-import React from 'react';
-// import styles from '../pages/game/Game.css';
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import styles from '../pages/game/Game.module.css';
 
 import waldo from '../assets/characters/waldo.webp';
 import odlaw from '../assets/characters/odlaw.webp';
 import whitebeard from '../assets/characters/whitebeard.webp';
 import wilma from '../assets/characters/wilma.jpg'
 
-const characters = [waldo, odlaw, whitebeard, wilma]
+const characters = [{name: 'waldo', url: waldo},
+    {name: 'odlaw', url: odlaw},
+    {name: 'whitebeard', url: whitebeard},
+    {name: 'wilma', url: wilma}];
+
+const keys = [
+  Math.random(),
+  Math.random(),
+  Math.random(),
+  Math.random(),
+]
 
 const Dropdown = () => {
+  const [imageSelected, setImageSelected] = useState(null);
+  
+  const handleClick = (e) => {
+    const newImage = e.target;
+    setImageSelected(newImage);
+  }
+
   return (
-    <div className='menu'>
+    <div className={styles.menu}>
         <h2>Which character?</h2>
         <div>
-          {characters.map(element => {
-            return <img 
-              key={element} 
-              src={element} 
-            />
+          {characters.map((element, index) => {
+            return (<img
+              key={keys[index]}
+              src={element.url}
+              onClick={handleClick}
+              className={classNames(styles.char,
+                  imageSelected && 
+                  imageSelected.src.includes(element.name) &&
+                   styles.selected
+                )}
+            />)
           })}
         </div>
     </div>
   )
 }
 
-export default Dropdown
+export default Dropdown;
--- frontend/src/pages/game/Game.jsx
+++ frontend/src/pages/game/Game.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+
 import styles from './Game.module.css';
 import image from '../../assets/game.png';
-import Clock from '../../components/Clock';
-import Dropdown from '../../components/Dropdown';
+import Clock from '../../components/Clock.jsx';
+import Dropdown from '../../components/Dropdown.jsx';
 
 const Game = () => {
   return (
-    <div>
+    <div className={styles.container}>
         <Clock />
         <img className={styles.play_image} src={image} onClick={onclick} />
         <Dropdown />
